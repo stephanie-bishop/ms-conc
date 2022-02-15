394769ff2a4324ee3f52b0af61e850b8d6800a2e
Soren Wacker <soerendip@gmail.com>
2022-02-15 12:58:02 -0700

    tidy up and request for docs

--- ms_mint_conc/ConcentrationEstimator.py
+++ ms_mint_conc/ConcentrationEstimator.py
@@ -4,11 +4,12 @@ import pandas as pd
 
 class ConcentrationEstimator:
     def __init__(self):
+        """
+        Please add docstrings...
+        """
         self.params_ = pd.DataFrame()
         self.interval = [0.5, 2]
 
-    #     def fit(self, X, y):
-    #         self.params_ = cc.calibration_curves( X , y)
     def set_interval(self, interval):
         self.interval = interval
 
@@ -25,17 +26,6 @@ class ConcentrationEstimator:
     def predict(self, X):
         return cc.transform(X, self.params_)
 
-    #         pass
-
     def score(self, y_pred, y_true):
-        #         X is the matrix of standard curves.....
+        # X is the matrix of standard curves.....
         return cc.fitting_score(y_pred, y_true)
-
-
-#         pass
-
-#     def load(self, fn = 'MINT-conc-parameters.csv'):
-#         self.params_ = pd.read_csv(fn)
-
-#     def save(self, fn = 'MINT-conc-parameters.csv')
-#         self.params_.to_csv(fn)
